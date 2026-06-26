8949f49c982adc90bbb78c990862e1bde3ac9dfc
ice198 <175214179+ice198@users.noreply.github.com>
2026-06-26 15:44:57 +0900

    update

--- configuration.nix
+++ configuration.nix
@@ -67,6 +67,25 @@
   #    "noatime"
   #  ];
   #};
+  #
+  # services.snapper = {
+  #snapshotInterval = "daily";
+  #cleanupInterval = "1d";
+  #configs = {
+  #  root = {
+  #    SUBVOLUME = "/";
+  #    ALLOW_GROUPS = [ "wheel" ];
+  #    TIMELINE_CREATE = true;
+  #    TIMELINE_CLEANUP = true;
+  #    TIMELINE_MIN_AGE = "1800";
+  #    TIMELINE_LIMIT_HOURLY = "0";
+  #    TIMELINE_LIMIT_DAILY = "30";
+  #    TIMELINE_LIMIT_WEEKLY = "0";
+  #    TIMELINE_LIMIT_MONTHLY = "0";
+  #    TIMELINE_LIMIT_YEARLY = "0";
+  #  };
+  #};
+  #};
 
   # Open port
   networking.firewall.allowedTCPPorts = [
